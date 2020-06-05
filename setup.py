9e617342a14a89ecd2c334208bdbb9de92fa5d0d
Breno Brandão <breno.brandao@celeritech.com.br>
2020-06-05 20:00:33 -0300

    build: update metadata

--- setup.py
+++ setup.py
@@ -8,9 +8,23 @@ version = None
 with open(root_dir / 'VERSION') as f:
     version = f.read()
 
+with open(root_dir / 'README.md') as f:
+    long_description = f.read()
+
 setup(
-    name='Compact',
+    name='compact-lrabbt',
     version=version,
+    author='Breno Brandão',
+    author_email='lrabbt@gmail.com',
+    description='Merges two csv files for unknown reasons',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lrabbt/compact',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Posix :: Linux',
+    ],
     packages=find_packages(),
     install_requires=[
         'SQLAlchemy==1.3.17',
@@ -21,5 +35,6 @@ setup(
             'compact = compact.cli:main',
             'compact-utils = compact.cli:utils'
         ]
-    }
+    },
+    python_requires='>=3.8',
 )
